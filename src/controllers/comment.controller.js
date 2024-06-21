a67b0b6987941054fc5db207fd84ee5906947860
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2024-06-21 18:53:43 +0530

    create "add comment" controller

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -1,18 +1,46 @@
 import mongoose from "mongoose"
-import {Comment} from "../models/comment.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import { Comment } from "../models/comment.model.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
 
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
-    const {videoId} = req.params
-    const {page = 1, limit = 10} = req.query
+    const { videoId } = req.params
+    const { page = 1, limit = 10 } = req.query
 
 })
 
 const addComment = asyncHandler(async (req, res) => {
     // TODO: add a comment to a video
+
+    const userId = req.user._id;
+
+    if (!userId) {
+        throw new ApiError(400, "You must log in to add a comment!");
+    }
+
+    const { videoId } = req.params;
+
+    if (!mongoose.isValidObjectId(videoId)) {
+        throw new ApiError(400, "Invalid video id");
+    }
+
+    const { content } = req.body;
+
+    if (!content) {
+        throw new ApiError(400, "Please provide content");
+    }
+
+    const comment = await Comment.create({
+        content,
+        owner: userId,
+        video: videoId
+    });
+
+    await comment.save();
+    return res.status(200).json(new ApiResponse(200, comment, "Comment added successfully"))
+
 })
 
 const updateComment = asyncHandler(async (req, res) => {
@@ -24,8 +52,8 @@ const deleteComment = asyncHandler(async (req, res) => {
 })
 
 export {
-    getVideoComments, 
-    addComment, 
+    getVideoComments,
+    addComment,
     updateComment,
-     deleteComment
-    }
+    deleteComment
+}

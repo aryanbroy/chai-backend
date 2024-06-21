4ce514ef2d1d42fa2578dac20719980ccc49dafe
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2024-06-21 18:15:06 +0530

    add endpoint to toggle video like

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -1,22 +1,58 @@
-import mongoose, {isValidObjectId} from "mongoose"
-import {Like} from "../models/like.model.js"
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import mongoose, { isValidObjectId } from "mongoose"
+import { Like } from "../models/like.model.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+import { Video } from "../models/video.model.js"
 
 const toggleVideoLike = asyncHandler(async (req, res) => {
-    const {videoId} = req.params
     //TODO: toggle like on video
+    // check if user is logged in or not
+    // get video id from params
+    // check if video id is valid or not
+    // check if user has already liked the video or not
+    // toggle like
+
+    const userId = req.user._id;
+
+    if (!userId) {
+        throw new ApiError(400, "Log in to like a video");
+    }
+
+    const { videoId } = req.params
+
+    const video = await Video.findById(videoId);
+
+    if (!video) {
+        throw new ApiError(404, "Video not found");
+    }
+
+    const like = await Like.findOne({
+        video: videoId,
+        likedBy: userId
+    });
+
+    if (like) {
+        await Like.findByIdAndDelete(like._id);
+        return res.status(200).json(new ApiResponse(200, [], "Like removed successfully"));
+    }
+    const newLike = await Like.create({
+        video: videoId,
+        likedBy: userId
+    })
+    await newLike.save();
+    return res.status(200).json(new ApiResponse(200, newLike, "Like added successfully"));
+
 })
 
 const toggleCommentLike = asyncHandler(async (req, res) => {
-    const {commentId} = req.params
+    const { commentId } = req.params
     //TODO: toggle like on comment
 
 })
 
 const toggleTweetLike = asyncHandler(async (req, res) => {
-    const {tweetId} = req.params
+    const { tweetId } = req.params
     //TODO: toggle like on tweet
 }
 )

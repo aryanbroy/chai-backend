f4a474dd6bf1368aab84b05b3b639011f9755978
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2024-06-30 23:21:52 +0530

    update app.js to return error as json

--- chai-backend/src/app.js
+++ chai-backend/src/app.js
@@ -9,10 +9,12 @@ app.use(cors({
     credentials: true
 }))
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
+
+app.use(express.json({ limit: "16kb" }))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
+
 
 
 //routes import
@@ -38,5 +40,15 @@ app.use("/api/v1/playlist", playlistRouter)
 app.use("/api/v1/dashboard", dashboardRouter)
 
 // http://localhost:8000/api/v1/users/register
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || 'Internal Server Error';
+    res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
 
 export { app }

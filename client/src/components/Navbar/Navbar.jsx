9bb05e537bbba961bc5ae8e08243089d66e7c9bf
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2024-07-01 23:06:57 +0530

    dynamically show user avatar or login button

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -5,9 +5,12 @@ import { IoNotifications } from "react-icons/io5";
 import { Button, TextField } from '@mui/material'
 import { FaSearch } from "react-icons/fa";
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 
 export default function Navbar() {
+    const { currentUser } = useSelector((state) => state.user);
+
     return (
         <div className={styles.navMainDiv}>
             <div className={styles.navInnerDiv}>
@@ -31,15 +34,19 @@ export default function Navbar() {
                     <Button variant='text' sx={{ border: "1px solid #848482", borderTopRightRadius: "25px", borderBottomRightRadius: "25px", color: "#A9A9A9", backgroundColor: "#343434", marginLeft: "-5px" }}><FaSearch size={28} /></Button>
                 </div>
                 <div className={styles.rightSideNav}>
-                    <p><RiVideoUploadFill size={30} /></p>
-                    <p><IoNotifications size={30} /></p>
-                    <Link to={"/login"} style={{ color: "white" }}>
-                        <Button variant="contained" sx={{ width: "100px", height: "40px", fontSize: "15px", fontWeight: "700" }}>
-
-                            Login
-
-                        </Button>
-                    </Link>
+                    <RiVideoUploadFill size={30} />
+                    <IoNotifications size={30} />
+                    {currentUser ? (
+                        <div className={styles.avatarDiv}>
+                            <img src={currentUser?.avatar} alt="avatar" className={styles.avatar} />
+                        </div>
+                    ) : (
+                        <Link to={"/login"} style={{ color: "white" }}>
+                            <Button variant="contained" sx={{ width: "100px", height: "40px", fontSize: "15px", fontWeight: "700" }}>
+                                Login
+                            </Button>
+                        </Link>
+                    )}
                 </div>
             </div>
         </div>

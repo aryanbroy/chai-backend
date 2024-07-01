c58a7e52bbfc3edd85224cfd0f5a5e4e37a40a33
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2024-07-01 20:42:14 +0530

    add redux persist

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,15 +5,18 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import App from './App.jsx'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { store } from './redux/store.js';
+import { store, persistor } from './redux/store.js';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react'
 
 const queryCLient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <QueryClientProvider client={queryCLient}>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </QueryClientProvider>
 )

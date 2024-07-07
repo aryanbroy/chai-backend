222e8803da79d4e550641878985e2292cdf33723
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2024-07-08 01:34:47 +0530

    add endpoint to get comments of a video

--- chai-backend/src/controllers/comment.controller.js
+++ chai-backend/src/controllers/comment.controller.js
@@ -3,11 +3,33 @@ import { Comment } from "../models/comment.model.js"
 import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
+import { Video } from "../models/video.model.js"
 
 const getVideoComments = asyncHandler(async (req, res) => {
     //TODO: get all comments for a video
     const { videoId } = req.params
     const { page = 1, limit = 10 } = req.query
+
+    const options = {
+        page: parseInt(page),
+        limit: parseInt(limit)
+    }
+
+    // const video = await Video.findById(videoId);
+
+    let aggregate = Comment.aggregate([
+        {
+            $match: {
+                video: new mongoose.Types.ObjectId(videoId)
+            }
+        }
+    ]);
+    Comment.aggregatePaginate(aggregate, options, (err, result) => {
+        if (err) {
+            throw new ApiError(401, err.message);
+        }
+        return res.status(200).json(new ApiResponse(200, result, "Comments found successfully"));
+    })
 
 })
 

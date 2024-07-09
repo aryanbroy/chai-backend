c84c204f55e7b5d1efcafde2a198b01b5f96c712
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2024-07-09 16:12:38 +0530

    fetch subscription videos of an user

--- client/src/pages/Subscription/Subscription.jsx
+++ client/src/pages/Subscription/Subscription.jsx
@@ -1,9 +1,106 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import styles from "./Subscription.module.css";
+import { useInfiniteQuery } from '@tanstack/react-query'
+import { useIntersection } from '@mantine/hooks'
+import { Text, Paper, Box, MantineProvider } from '@mantine/core';
+import { ColorRing } from 'react-loader-spinner';
+import axios from 'axios';
+import { useNavigate, useParams } from 'react-router-dom'
+import { FaPlay } from "react-icons/fa";
 
 export default function Subscription() {
+
+    const [subscribedVideos, setSubscribedVideos] = useState([])
+    const [loading, setLoading] = useState(false)
+    const navigate = useNavigate();
+    const fetchSubscribedVideos = async () => {
+        try {
+            setLoading(true)
+            const res = await axios.get(`/api/subscriptions/getSubscriptionVideos`, { withCredentials: true });
+            const data = res.data;
+            // console.log(data);
+            setSubscribedVideos(data.data)
+            setLoading(false)
+        } catch (error) {
+            console.log(error)
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        fetchSubscribedVideos()
+    }, [])
+
+    const handleVideoClick = async (post) => {
+        navigate(`/watch/${post?._id}`);
+        try {
+            const res = await axios.patch(`/api/videos/increase/view/${post?._id}`, {}, { withCredentials: true });
+            const data = res.data;
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     return (
-        <div>
-            Subscription
+        <div className={styles.mainDiv}>
+            <div className={styles.innerDiv}>
+                {subscribedVideos?.map((post, i) => {
+                    return (
+                        <div key={post?._id} className={styles.individualVideoDiv} onClick={() => handleVideoClick(post)}>
+                            <Card sx={{ width: 375, backgroundColor: "#343434", boxShadow: "none" }} key={post?.id}>
+                                <div className={styles.cardMediaWrapper}>
+                                    <CardMedia
+                                        sx={{ height: 230, borderRadius: "16px" }}
+                                        image={post?.thumbnail}
+                                        title={post?.title}
+                                        className={styles.cardMedia}
+                                    />
+                                    <div className={styles.overlay}>
+                                        <div className={styles.playDiv}>
+                                            <FaPlay size={15} style={{ marginTop: "-2px" }} />
+                                            <p className={styles.playPara}>Play</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <CardContent>
+                                    <Typography gutterBottom variant="h5" component="div" sx={{ color: "white" }}>
+                                        {post?.title}
+                                    </Typography>
+                                    <Typography variant="body2" color="text.secondary" sx={{ color: "white" }}>
+                                        {post?.description}
+                                    </Typography>
+                                </CardContent>
+                            </Card>
+                        </div>
+                    )
+                })}
+            </div>
+            <div className={styles.loadingDiv}>
+                {/* {isFetchingNextPage
+                            ? "Loding more.."
+                            : (data?.pages.length ?? 0) < 3
+                                ? "Load more"
+                                : "Nothing to load"
+                        } */}
+
+                {(loading) && (
+                    <ColorRing
+                        visible={true}
+                        height="80"
+                        width="80"
+                        ariaLabel="color-ring-loading"
+                        wrapperStyle={{}}
+                        wrapperClass="color-ring-wrapper"
+                        colors={["#A9A9A9", "#A9A9A9", "#A9A9A9", "#A9A9A9", "#A9A9A9"]}
+                    />
+                )}
+            </div>
         </div>
     )
 }

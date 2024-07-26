8e85369b52e5772298cc3b94f5de30027bffee1f
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2024-07-27 00:20:08 +0530

    handle playlist error when no videos are present in playlist

--- client/src/components/Playlist/Playlist.jsx
+++ client/src/components/Playlist/Playlist.jsx
@@ -12,12 +12,14 @@ export default function Playlist() {
 
     const [videosData, setVideosData] = useState(null);
     const [followers, setFollowers] = useState(null);
+    const [isFetching, setIsFetching] = useState(false);
 
     const { playlistId } = useParams();
     const navigate = useNavigate();
     // console.log(videosData)
 
     const fetchPlaylistData = async () => {
+        setIsFetching(true)
         try {
             const res = await axios.get(`/api/playlist/${playlistId}`, { withCredentials: true });
             const data = res.data;
@@ -35,6 +37,8 @@ export default function Playlist() {
             setVideosData(data.data);
         } catch (error) {
             console.log(error)
+        } finally {
+            setIsFetching(false);
         }
     }
 
@@ -68,63 +72,84 @@ export default function Playlist() {
     }
 
     return (
-        <div className={styles.playListMainDiv}>
-            <div className={styles.leftDiv}>
-                <div className={styles.playlistCard}>
-                    <div className={styles.imageContainer}>
-                        <img className={styles.playlistImage} src={videosData?.videos[0].thumbnail} alt="" />
-                    </div>
-                    <div className={styles.playlistInfo}>
-                        <div className={styles.playlistTitle}>{videosData?.name}</div>
-                        <div className={styles.playlistMeta}>
-                            <span>100k Views</span>
-                            <span>•</span>
-                            <span>{videosData?.playListCreatedTimeAgo}</span>
-                        </div>
-                        <div className={styles.playlistVideos}>{videosData?.videos?.length} videos</div>
-                    </div>
-                </div>
-                <div className={styles.descriptionDiv}>
-                    <p className={styles.description}>{videosData?.description}</p>
-                    <div className={styles.ownerDiv}>
-                        <img className={styles.ownerImage} src={videosData?.owner?.avatar} alt="" />
-                        <div>
-                            <p className={styles.ownerPara}>{videosData?.owner?.username}</p>
-                            <p className={styles.ownerPara}>{followers && `${followers} followers`} </p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div className={styles.rightDiv}>
-                {videosData?.videos?.map((video, i) => (
-                    <div key={i} onClick={() => handleClick(video)}>
-                        <Card sx={{ display: 'flex', width: "100%", gap: "0.7rem", background: "transparent", boxShadow: "none", marginBottom: "1rem", "&:hover": { cursor: "pointer" } }}>
-                            <CardMedia
-                                component="img"
-                                sx={{ width: 250, height: 130, borderRadius: "8px" }}
-                                image={video?.thumbnail}
-                                alt="Live from space album cover"
-                            />
-                            <CardContent sx={{ flex: '1 0 auto', background: "transparent" }}>
-                                <Typography component="div" variant="h5" sx={{ color: "white" }}>
-                                    {video?.title}
-                                </Typography>
-                                <Typography variant='subtitle1' component="p" sx={{ padding: "0", color: "rgba(255,255,255,0.9)", fontSize: "14px" }}>
-                                    {video?.owner.username}
-                                </Typography>
-                                <div style={{ display: "flex", gap: "0.8rem" }}>
-                                    <Typography variant="subtitle1" component="p" sx={{ padding: "0", color: "#AAAAAA", fontSize: "14px" }}>
-                                        {video?.views} views
-                                    </Typography>
-                                    <Typography variant="subtitle1" component="p" sx={{ padding: "0", color: "#AAAAAA", fontSize: "14px" }}>
-                                        {video?.uploadedTimeAgo}
-                                    </Typography>
+        <>
+
+            {videosData?.videos.length > 0 ? (
+                <div className={styles.playListMainDiv}>
+                    <div className={styles.leftDiv}>
+                        <div className={styles.playlistCard}>
+                            <div className={styles.imageContainer}>
+                                <img className={styles.playlistImage} src={videosData?.videos[0]?.thumbnail} alt="" />
+                            </div>
+                            <div className={styles.playlistInfo}>
+                                <div className={styles.playlistTitle}>{videosData?.name}</div>
+                                <div className={styles.playlistMeta}>
+                                    <span>100k Views</span>
+                                    <span>•</span>
+                                    <span>{videosData?.playListCreatedTimeAgo}</span>
                                 </div>
-                            </CardContent>
-                        </Card>
+                                <div className={styles.playlistVideos}>{videosData?.videos?.length} videos</div>
+                            </div>
+                        </div>
+                        <div className={styles.descriptionDiv}>
+                            <p className={styles.description}>{videosData?.description}</p>
+                            <div className={styles.ownerDiv}>
+                                <img className={styles.ownerImage} src={videosData?.owner?.avatar} alt="" />
+                                <div>
+                                    <p className={styles.ownerPara}>{videosData?.owner?.username}</p>
+                                    <p className={styles.ownerPara}>{followers && `${followers} followers`} </p>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                ))}
-            </div>
-        </div>
+                    <div className={styles.rightDiv}>
+                        {videosData?.videos?.map((video, i) => (
+                            <div key={i} onClick={() => handleClick(video)}>
+                                <Card sx={{ display: 'flex', width: "100%", gap: "0.7rem", background: "transparent", boxShadow: "none", marginBottom: "1rem", "&:hover": { cursor: "pointer" } }}>
+                                    <CardMedia
+                                        component="img"
+                                        sx={{ width: 250, height: 130, borderRadius: "8px" }}
+                                        image={video?.thumbnail}
+                                        alt="Live from space album cover"
+                                    />
+                                    <CardContent sx={{ flex: '1 0 auto', background: "transparent" }}>
+                                        <Typography component="div" variant="h5" sx={{ color: "white" }}>
+                                            {video?.title}
+                                        </Typography>
+                                        <Typography variant='subtitle1' component="p" sx={{ padding: "0", color: "rgba(255,255,255,0.9)", fontSize: "14px" }}>
+                                            {video?.owner.username}
+                                        </Typography>
+                                        <div style={{ display: "flex", gap: "0.8rem" }}>
+                                            <Typography variant="subtitle1" component="p" sx={{ padding: "0", color: "#AAAAAA", fontSize: "14px" }}>
+                                                {video?.views} views
+                                            </Typography>
+                                            <Typography variant="subtitle1" component="p" sx={{ padding: "0", color: "#AAAAAA", fontSize: "14px" }}>
+                                                {video?.uploadedTimeAgo}
+                                            </Typography>
+                                        </div>
+                                    </CardContent>
+                                </Card>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            ) : (
+                !isFetching && (
+                    <div style={{ display: "flex", justifyContent: "center", paddingTop: "9rem", paddingRight: '10rem' }}>
+                        <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+                            <img
+                                className={styles.noVideoImg}
+                                src='/images/islandQuality.png'
+                                alt="empty_island"
+                            />
+                            <div className={styles.noVideoTextDiv}>
+                                <h2 className={styles.noVideoHeading} >Looks so empty!</h2>
+                                <p className={styles.noVideoPara}>Start adding some videos to remove this emptiness!</p>
+                            </div>
+                        </div>
+                    </div>
+                )
+            )}
+        </>
     )
 }
--- client/src/pages/Playlists/Playlists.jsx
+++ client/src/pages/Playlists/Playlists.jsx
@@ -3,21 +3,26 @@ import styles from "./Playlists.module.css"
 import axios from 'axios';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { Skeleton } from '@mui/material'
 
 export default function Playlists() {
 
     const { currentUser } = useSelector((state) => state.user);
     const [userPlaylists, setUserPlaylists] = useState([]);
+    const [isFetchingPlaylists, setIsFetchingPlaylists] = useState(false);
     // console.log(userPlaylists)
     const navigate = useNavigate();
 
     const fetchPlaylists = async () => {
+        setIsFetchingPlaylists(true)
         try {
             const res = await axios.get(`/api/playlist/user/${currentUser?._id}`, { withCredentials: true });
             const data = res.data;
             setUserPlaylists(data.data);
         } catch (error) {
             console.log(error)
+        } finally {
+            setIsFetchingPlaylists(false)
         }
     }
 
@@ -35,23 +40,32 @@ export default function Playlists() {
         <>
             {currentUser ? (
                 <div className={styles.playlistMainDiv}>
-                    {userPlaylists?.map((playlist, i) => (
-                        <div className={styles.playlistCard} key={i} onClick={() => handleClick(playlist?._id)}>
-                            <div className={styles.imageContainer}>
-                                <img className={styles.playlistImage} src={playlist?.videos[0]?.thumbnail} alt="" />
-                                <div className={styles.overlay}></div>
-                            </div>
-                            <div className={styles.playlistInfo}>
-                                <div className={styles.playlistTitle}>{playlist?.name}</div>
-                                <div className={styles.playlistMeta}>
-                                    <span>100k Views</span>
-                                    <span>•</span>
-                                    <span>18 hours ago</span>
+                    {isFetchingPlaylists ? (
+                        Array.from({ length: 20 }, (_, i) => (
+
+                            <Skeleton key={i} animation="wave" variant="rounded" width={350} height={240} sx={{ borderRadius: '10px', backgroundColor: '#2c2c2c' }} />
+                        ))
+                    ) : (
+
+                        userPlaylists?.map((playlist, i) => (
+                            <div className={styles.playlistCard} key={i} onClick={() => handleClick(playlist?._id)}>
+                                <div className={styles.imageContainer}>
+                                    <img className={styles.playlistImage} src={playlist?.videos[0]?.thumbnail} alt="" />
+                                    <div className={styles.overlay}></div>
                                 </div>
-                                <div className={styles.playlistVideos}>{playlist?.videos?.length} videos</div>
+                                <div className={styles.playlistInfo}>
+                                    <div className={styles.playlistTitle}>{playlist?.name}</div>
+                                    <div className={styles.playlistMeta}>
+                                        <span>100k Views</span>
+                                        <span>•</span>
+                                        <span>18 hours ago</span>
+                                    </div>
+                                    <div className={styles.playlistVideos}>{playlist?.videos?.length} videos</div>
+                                </div>
                             </div>
-                        </div>
-                    ))}
+                        ))
+
+                    )}
                 </div>
             ) : (
                 <div>

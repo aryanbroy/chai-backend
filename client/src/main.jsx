11ab21cd0b7da748771c32e9109e14034c7b0483
aryanbroy <136257977+aryanbroy@users.noreply.github.com>
2024-08-19 19:31:31 +0530

    change file name

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import App from './App.jsx'
-import './index.css'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import App from './App.jsx';
+import './index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { store, persistor } from './redux/store.js';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react'
+import { PersistGate } from 'redux-persist/integration/react';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
 const queryCLient = new QueryClient();
 
@@ -18,5 +21,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <App />
       </PersistGate>
     </Provider>
-  </QueryClientProvider>
-)
+  </QueryClientProvider>,
+);
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,24 +1,24 @@
-import { combineReducers, configureStore } from '@reduxjs/toolkit'
-import userReducer from "./user/userSlice.js";
-import storage from "redux-persist/lib/storage";
-import { persistReducer, persistStore } from 'redux-persist'
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice.js';
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 
 const rootReducer = combineReducers({
-    user: userReducer
-})
+  user: userReducer,
+});
 
 const persistConfig = {
-    key: "root",
-    storage,
-    version: 1
-}
+  key: 'root',
+  storage,
+  version: 1,
+};
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-
 export const store = configureStore({
-    reducer: persistedReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false })
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
+  devTools: false,
 });
 
 export const persistor = persistStore(store);
